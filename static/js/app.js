319b60ece10d7792599bf1249a678186ddbd6c5a
pkorat <pkorat@github.com>
2021-08-27 21:14:11 -0500

    bar chart done and bubble chart almost done

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,18 @@
 
-var indiviuals = d3.json('samples.json').then(function(data) { 
+var indiviuals_names = d3.json('samples.json').then(function(data) { 
     return data.names
 })
 
-var indiviual_descrs = d3.json('samples.json').then(function(data) {
+var indiviual_metadata = d3.json('samples.json').then(function(data) {
     return data.metadata
 })
 
-indiviuals.then(function(data) {
+var individual_samples = d3.json('samples.json').then(function(data) {
+    return data.samples
+})
+
+
+indiviuals_names.then(function(data) {
     
     for (var i=0; i<data.length; i++) {
         var elmt = document.createElement('option')
@@ -15,7 +20,6 @@ indiviuals.then(function(data) {
         document.getElementById('selDataset').appendChild(elmt)
     }
 })
-
 
 
 function optionChanged(val) {
@@ -29,7 +33,7 @@ function optionChanged(val) {
     var dataset = dropdownMenu.property('value')
 
     // promise.then() to get the data
-    indiviual_descrs.then(function(data) {
+    indiviual_metadata.then(function(data) {
 
         // find the element in the data array that is selected
         description = data.find(element => element.id == dataset)
@@ -42,5 +46,48 @@ function optionChanged(val) {
             elmt.appendChild(document.createTextNode(keys[i] + ': ' + values[i]))
             document.getElementById('sample-metadata').appendChild(elmt)
         }
+    })
+
+    // promise.then() to get the data
+    individual_samples.then(function(data) {
+
+        // find the element in the data array that is selected
+        indiviual = data.find(element => element.id == dataset)
+        keys = Object.keys(indiviual)
+        values = Object.values(indiviual)
+
+        // create data variable with x and y axis with hovertext (bar chart)
+        var data = {
+            x: values[2].slice(0, 10),
+            y: values[1].slice(0, 10).map(element => 'OTU ' + element),
+            type: 'bar',
+            text: values[3].slice(0, 10),
+            orientation: 'h'
+        }
+
+        // create layout variable to reverse the bar chart at the y axis (bar chart)
+        var layout = {
+            yaxis: {
+                autorange:'reversed'
+            }
+        }
+
+        // create a new plot at each new selection (bar chart)
+        Plotly.newPlot('bar', [data], layout)
+
+        // create data variable for x and y axis with hovertext (bubble chart)
+        var data = {
+            x: values[1],
+            y: values[2],
+            mode: 'markers',
+            text: values[3],
+            marker: {
+                size: values[2].map(element => element / 1.3),
+                color: values[1]
+            }
+        }
+
+        // create a new plot at each new selection (bubble chart)
+        Plotly.newPlot('bubble', [data])
     })
 }
